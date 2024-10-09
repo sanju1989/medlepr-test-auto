7970fc63eee423217ae637fbc04a624e7217be3d
sanjay <sharmaroyl123@gmail.com>
2024-10-09 12:53:43 +0530

    test

---
+++ src/main/java/com/example/medelpar_test_auto/MedelparTestAutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.medelpar_test_auto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedelparTestAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedelparTestAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medelpar_test_auto/MedelparTestAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medelpar_test_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedelparTestAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
